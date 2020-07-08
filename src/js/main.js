181b036fdf007376e79d87b53d12c03f1308121d
ann-phoenix <ann.phoenix.it@gmail.com>
2020-07-08 18:42:25 +0300

    added main section

---
+++ src/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+	$('.slider').slick({
+		dots: true,
+		autoplay: true,
+		autoplaySpeed: 5000,
+		fade: true,
+		prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/back.png" alt="prev arrow"></button>',
+    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/next.png" alt="next arrow"></button>',
+	});
+
+});
+
