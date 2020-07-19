994419ad51320395afc9a091f7fa6b782a8fd906
ann-phoenix <ann.phoenix.it@gmail.com>
2020-07-19 20:05:31 +0300

    added adaptive and burger menu

--- src/js/main.js
+++ src/js/main.js
@@ -8,7 +8,8 @@ $(function () {
 		autoplaySpeed: 5000,
 		fade: true,
 		prevArrow: '<button class="slick-arrow slick-prev"><img src="images/icons/back.png" alt="prev arrow"></button>',
-    nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/next.png" alt="next arrow"></button>',
+		nextArrow: '<button class="slick-arrow slick-next"><img src="images/icons/next.png" alt="next arrow"></button>',
+
 	});
 
 	$('.offers__inner').slick({
@@ -128,6 +129,23 @@ $(document).ready(function() {
 
 //burger menu
 
+$('.main__burger').on('click', function(){
+	$('.main__menu-list').slideToggle();
+ });
+
+$('.main__burger').click(function(event){
+	$('.main__burger').toggleClass('active');
+});
+
+
+
 //scroll
 
-//sticky header
+$(document).ready(function(){
+	$('a[href^="#"]').click(function(){
+		var target = $(this).attr('href');
+		$('html, body').animate({
+			scrollTop: $(target).offset().top
+		}, 1000);
+	});
+});
